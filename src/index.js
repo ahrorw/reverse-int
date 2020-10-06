f8a23b461ed1095e23d5a2d1bacbc8a0baaf1811
Akhror Sharafutdinov <rgras@mail.ru>
2020-10-06 16:41:31 +0300

    feat:complete Reverse-int Task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
+    if ((n === undefined) || (n === null)) return 0;
   
-}
+    let arrN = String(Math.abs(n)).split('');
+  
+    while (arrN[arrN.length-1] === '0') arrN.pop();
+  
+    return	eval(arrN.reverse().join(''));
+  }
